db41ec68326434432399aa3da4c03a362c355eaf
Jędrzej Grembowski <Stachua123@gmail.com>
2020-01-09 09:40:34 +0100

    showUsers, addUsers

--- bash/manage_user.sh
+++ bash/manage_user.sh
@@ -1,39 +1,57 @@
 #!/bin/bash
 
-function loadUsers() {
-    echo "LoadUsers ..."
-}
+
+#zmienna globalna
+#ładowanie danych z pliku
+user_list=(`cat users.txt`)
+
 function showUsers() {
-    echo "ShowUsers ..."
+    echo "LoadUsers ..."
+    for(( i=0; i<=${#user_list[@]}; i++ ))
+    do
+	echo "	${user_list[i]}"
+    done
 }
+
 function addUsers() {
     echo "AddUsers ..."
+    for user in "${user_list[@]}"
+    do
+	echo "Add user: ${user}"
+	sudo useradd  ${user} -s /sbin/nologin -g "users"
+    done
 }
+
 function delUsers() {
     echo "DelUsers ..."
 }
+
 function acceptRemoteLogin() {
     echo "AcceptRemoteLogin ..."
 }
+
 function deniedRemoteLogin() {
     echo "DeniedRemoteLogin ..."
+}
+
 function help() {
 cat << EndOfMessage
-    Opis opcji skrytpu
-    LU - ładowanie użytkownikó z pliku
-    Su duddd
+    Opis opcji skrytpu:
+    .................................
+    SU - listowanie użytkowników z pliku
+    AU - dodawanie użytkowników
+
 EndOfMessage
 }
-}
+
 function quit() {
     exit 0
 }
 
 #menu
-select option in LU SU AU DU ARL DRL HELP QUIT
+select option in SU AU DU ARL DRL HELP QUIT
 do
     case ${option} in
-      "LU") loadUsers ;;
       "SU") showUsers ;;
       "AU") addUsers ;;
       "DU") delUsers ;;
