dcc05c3e2b4e1bf9a29a221f0a51dd5ca6c3d888
Jędrzej Grembowski <Stachua123@gmail.com>
2020-01-15 11:31:34 +0100

    maanage user 2

--- bash/manage_user.sh
+++ bash/manage_user.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 
@@ -6,7 +7,7 @@
 user_list=(`cat users.txt`)
 
 function showUsers() {
-    echo "LoadUsers ..."
+    echo "showUsers ..."
     for(( i=0; i<=${#user_list[@]}; i++ ))
     do
 	echo "	${user_list[i]}"
@@ -15,15 +16,28 @@ function showUsers() {
 
 function addUsers() {
     echo "AddUsers ..."
-    for user in "${user_list[@]}"
-    do
-	echo "Add user: ${user}"
-	sudo useradd  ${user} -s /sbin/nologin -g "users"
-    done
+    echo -n "Are you sure? [y/n] "
+    read sure
+    if [ "${sure}" == "y" ]; then
+	for user in "${user_list[@]}"
+	do
+	    echo "Add user: ${user} [OK]"
+	    sudo useradd  ${user} -s /sbin/nologin -g "users"
+	done
+    fi
 }
 
 function delUsers() {
     echo "DelUsers ..."
+    echo -n "Are you sure? [y/n] "
+    read sure
+    if [ "${sure}" == "y" ]; then
+	for user in "${user_list[@]}"
+	do
+	    echo "Remove user: ${user} [OK]"
+	    sudo userdel ${user}
+	done
+    fi 
 }
 
 function acceptRemoteLogin() {
